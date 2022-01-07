c5e00c44f0b5a285799f607a8612762716b17772
gdhu <guodong_hu@126.com>
2022-01-07 18:07:53 +0800

    Chapter 2

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+    // let -> create a variable
+    // variable is immutable
+    // if you want to change, use mut xxx
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+        println!("You guessed: {}", guess);
+    
+    macth guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
