af1bc9409beb6b10ce566561d20f093fa546ba23
gdhu <guodong_hu@126.com>
2022-01-10 17:44:06 +0800

    Chapter2 Finished!!!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,25 +7,35 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
-    // let -> create a variable
-    // variable is immutable
-    // if you want to change, use mut xxx
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
+        // let -> create a variable
+        // variable is immutable
+        // if you want to change, use mut xxx
+        let mut guess = String::new();
     
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        // 覆盖了之前的guess string类型
+        let guess: u32 = match guess.trim().parse() {
+            // Result 返回 OK 或者 Err
+            Ok(num) => num,
+            Err(_) => continue,
+        };
     
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
 }
