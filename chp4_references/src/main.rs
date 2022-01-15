78340ea969726075bf9ee23fb657bc245bd83e38
gdhu <guodong_hu@126.com>
2022-01-15 22:32:47 +0800

    chapter4

--- chp4_references/src/main.rs
+++ chp4_references/src/main.rs
@@ -2,9 +2,20 @@ fn main() {
     let s = String::from("abcd");
     let len = calc_length(&s);
     println!("length is {}", len);
+    let mut s = String::from("Hello");
+    change(&mut s);
+    println!("{}", s);
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+    println!("{}{}", r1, r2);
 }
 
 
 fn calc_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str("hello!");
 }
