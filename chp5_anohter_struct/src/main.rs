5c886bdbb3914c417f62de98ebe942a481ef40a7
gdhucoder <gdong.hu@gmail.com>
2022-01-17 01:17:50 +0800

    Chapter5

--- chp5_anohter_struct/src/main.rs
+++ chp5_anohter_struct/src/main.rs
@@ -7,16 +7,50 @@ fn main() {
     // let dim = (17, 17);
     // println!("{}", area(dim));
     let ret1 = Rectangle {
-        width: 88,
+        width: dbg!(88),
         height: 89
     };
 
     println!("{}", area(&ret1));
+
+    println!("{:#?}", ret1);
+
+    dbg!(&ret1);
+
+    println!("{}", ret1.area());
+
+    let other = Rectangle {
+        width: 100,
+        height: 99,
+    };
+
+    println!("can hold {}", ret1.can_hold(&other));
+
+    let sq1 = Rectangle::square(100);
+    println!("square is {:#?}", sq1);
 }
 
+#[derive(Debug)]
 struct  Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        return self.height > other.height && self.width > other.width
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
 }
 
 fn area(rec: &Rectangle) -> u32 {
