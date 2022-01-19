99b9debf4c2e8a3da14ea3da385624ba21132c84
gdhu <guodong_hu@126.com>
2022-01-19 17:54:33 +0800

    chapter6

--- chp6_option/src/main.rs
+++ chp6_option/src/main.rs
@@ -4,6 +4,29 @@ fn main() {
     let penny = Coin::Penny;
     println!("{}", value_in_cents(penny));
     println!("{}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => {println!("hello 3")},
+        // other situation
+        _ => reroll(),
+    }
+}
+
+fn reroll() {
+    println!("please reroll");
+}
+
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
 }
 
 #[derive(Debug)]
