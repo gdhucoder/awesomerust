e643e393d44eae5f452caf12a0cee0a418cddaf5
gdhu <guodong_hu@126.com>
2022-02-05 14:00:16 +0800

    chp15

---
+++ chp15_deref/src/main.rs
@@ -0,0 +1,44 @@
+use std::ops::Deref;
+
+fn main() {
+    let x = 10;
+    let y = &x;
+
+    assert_eq!(10, x);
+    println!("{}", y);
+    assert_eq!(10, *y);
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("hello, {}", name);
+}
