0237a32afa830ffbfe15462b1b65022a873ee3b4
gdhu <guodong_hu@126.com>
2022-02-05 14:27:35 +0800

    chp15 reference count

--- chp15_box/src/main.rs
+++ chp15_box/src/main.rs
@@ -1,10 +1,21 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
-
+use std::rc::Rc;
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Nil))));
+    // let list = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Nil)))));
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating a = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
 }
