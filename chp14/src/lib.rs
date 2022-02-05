702d1b0aebdfadf2b9e8e102190eed9dc527f660
gdhu <guodong_hu@126.com>
2022-02-05 08:53:22 +0800

    chp14

---
+++ chp14/src/lib.rs
@@ -0,0 +1,34 @@
+//! # Art
+//! 
+//! A library for modeling artisitc concepts.
+
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
+pub mod kinds {
+
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use super::kinds::*;
+
+    /// Combines two primary colors in equal amounts
+    /// to create a secondary color.
+    pub fn mix (c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        SecondaryColor::Green
+    }
+}
---
+++ chp14/src/main.rs
@@ -0,0 +1,8 @@
+use chp14::mix;
+use chp14::PrimaryColor;
+fn main() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+
+    mix(red, yellow);
+}
