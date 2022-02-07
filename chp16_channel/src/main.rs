9a916ab02ea9321f7f7eda2c30580a0fcc92c9f6
gdhu <guodong_hu@126.com>
2022-02-07 18:21:29 +0800

    chp16

---
+++ chp16_channel/src/main.rs
@@ -0,0 +1,18 @@
+use std::sync::mpsc;
+use std::thread;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        // wrong!
+        // println!("val is {}", val);
+    });
+
+    // receiver has the ownership!
+    let received = rx.recv().unwrap();
+
+    println!("Got: {}", received);
+}
