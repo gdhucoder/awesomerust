75d85d831081015a41a348d474bf479c63c6e218
hugd <guodong_hu@126.com>
2022-02-10 18:00:39 +0800

    update chp17

---
+++ chp17_oop/src/lib.rs
@@ -0,0 +1,22 @@
+pub struct AveragedCollection {
+    list: Vec<i32>, 
+    average: f64,
+}
+
+impl AveragedCollection {
+
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+}
