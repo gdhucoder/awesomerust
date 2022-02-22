6812c0c50345e96db433b0252b70fcdd34282741
gdhucoder <gdong.hu@gmail.com>
2022-02-23 06:06:56 +0800

    update

--- chp20_multithreadswebserver/src/main.rs
+++ chp20_multithreadswebserver/src/main.rs
@@ -4,10 +4,11 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::time::Duration;
 use std::thread;
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = Thread::new(4);
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
