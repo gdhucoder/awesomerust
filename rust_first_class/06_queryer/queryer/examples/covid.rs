7db2244899be213180833b3796dac2cae8781e7c
gdhucoder <gdong.hu@gmail.com>
2022-03-02 09:34:04 +0800

    update

--- rust_first_class/06_queryer/queryer/examples/covid.rs
+++ rust_first_class/06_queryer/queryer/examples/covid.rs
@@ -2,13 +2,12 @@
 use anyhow::Result;
 use polars::prelude::*;
 use std::io::Cursor;
-use reqwest::header::REFERER;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    
+
     let url = "https://raw.github.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv";
     let data = reqwest::get(url).await?.text().await?;
 
